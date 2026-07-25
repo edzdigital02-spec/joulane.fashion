f507bc53e4451051912aae2897a88463ff115422
Joulane <joulane@fashion.com>
2026-07-25 23:31:24 +0100

    Add vite.config.js for multi-page build

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        admin: resolve(__dirname, 'admin.html'),
+        stock: resolve(__dirname, 'stock.html'),
+      },
+    },
+  },
+});
